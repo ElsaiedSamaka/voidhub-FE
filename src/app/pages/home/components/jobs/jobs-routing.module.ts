d7c7c265a41bfe248f3dd50d6b3e88d8125043ea
Saied <elsaied.samaka1997@gmail.com>
2023-08-11 18:41:14 +0300

    breakpoint 1

--- src/app/pages/home/components/jobs/jobs-routing.module.ts
+++ src/app/pages/home/components/jobs/jobs-routing.module.ts
@@ -6,13 +6,13 @@ const routes: Routes = [
   {
     path: '',
     component: IndexComponent,
-    // children: [
-    //   // { path: '', component: HomeComponent },
-    //   // { path: 'jobs' },
-    //   // { path: 'companies', component: CompaniesComponent },
-    //   // { path: 'messages', component: MessagesComponent },
-    //   // { path: 'reading-list', component: ReadingListComponent },
-    // ],
+    children: [
+      // { path: '', component: HomeComponent },
+      // { path: 'jobs' },
+      // { path: 'companies', component: CompaniesComponent },
+      // { path: 'messages', component: MessagesComponent },
+      // { path: 'reading-list', component: ReadingListComponent },
+    ],
   },
 ];
 
--- src/app/pages/home/components/jobs/jobs.module.ts
+++ src/app/pages/home/components/jobs/jobs.module.ts
@@ -1,9 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { IndexComponent } from './components/index/index.component';
 import { JobsRoutingModule } from './jobs-routing.module';
 
 @NgModule({
-  imports: [CommonModule, JobsRoutingModule],
-  declarations: [],
+  imports: [CommonModule, SharedModule, JobsRoutingModule],
+  declarations: [IndexComponent],
 })
 export class JobsModule {}
