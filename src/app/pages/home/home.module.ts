d72443af779967081db76648062a4c456f8abc3b
Saied <elsaied.samaka1997@gmail.com>
2023-08-11 11:44:23 +0300

    breakpoint 1

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { HomeComponent } from './components/home/home.component';
 import { IndexComponent } from './components/index/index.component';
 import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   imports: [CommonModule, SharedModule, HomeRoutingModule],
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, HomeComponent],
 })
 export class HomeModule {}
