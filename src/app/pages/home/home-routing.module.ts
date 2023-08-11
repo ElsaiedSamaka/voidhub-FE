a8a4be4fa8e9dca7366087ffd71bfc541ed7d40a
Saied <elsaied.samaka1997@gmail.com>
2023-08-11 11:33:23 +0300

    breakpoint 1

--- src/app/pages/home/home-routing.module.ts
+++ src/app/pages/home/home-routing.module.ts
@@ -2,9 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 // import { ProductResolverService } from 'src/core/resolvers/product-resolver.service';
 // import { AllProductsComponent } from './components/all-products/all-products.component';
+import { CompaniesComponent } from './components/companies/companies.component';
 import { HomeComponent } from './components/home/home.component';
 import { IndexComponent } from './components/index/index.component';
 import { JobsComponent } from './components/jobs/jobs.component';
+import { MessagesComponent } from './components/messages/messages.component';
+import { ReadingListComponent } from './components/reading-list/reading-list.component';
 // import { NotFoundComponent } from './components/not-found/not-found.component';
 // import { ProductDetailsComponent } from './components/product-details/product-details.component';
 
@@ -15,6 +18,9 @@ const routes: Routes = [
     children: [
       { path: '', component: HomeComponent },
       { path: 'jobs', component: JobsComponent },
+      { path: 'companies', component: CompaniesComponent },
+      { path: 'messages', component: MessagesComponent },
+      { path: 'reading-list', component: ReadingListComponent },
     ],
   },
   // {
