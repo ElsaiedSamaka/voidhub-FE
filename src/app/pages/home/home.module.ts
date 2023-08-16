e3180e821e7faa68e1a7af02c7d4042e884a2ace
Saied <elsaied.samaka1997@gmail.com>
2023-08-16 23:52:52 +0300

    breakpoint 8

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,12 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { CompaniesComponent } from './components/companies/companies.component';
 import { HomeComponent } from './components/home/home.component';
 import { IndexComponent } from './components/index/index.component';
 import { HomeRoutingModule } from './home-routing.module';
+import { ViewsModule } from './views/views.module';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, HomeRoutingModule],
-  declarations: [IndexComponent, HomeComponent],
+  imports: [CommonModule, SharedModule, ViewsModule, HomeRoutingModule],
+  declarations: [IndexComponent, HomeComponent, CompaniesComponent],
 })
 export class HomeModule {}
--- src/app/pages/home/views/views.module.ts
+++ src/app/pages/home/views/views.module.ts
@@ -1,8 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { CompanyCardComponent } from './company-card/company-card.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [],
+  declarations: [CompanyCardComponent],
+  exports: [CompanyCardComponent],
 })
 export class ViewsModule {}
