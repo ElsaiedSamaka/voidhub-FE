6b50c659a76908756eaf90cb0ad81c64b4a835a2
Saied <elsaied.samaka1997@gmail.com>
2023-08-16 08:56:00 +0300

    breakpoint 1

--- src/app/pages/home/components/index/index.component.ts
+++ src/app/pages/home/components/index/index.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/core/services/auth.service';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
-
-  constructor() { }
+  currentUser: any;
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {
+    this.authService.currentUser$.subscribe({
+      next: (currentUser) => {
+        this.currentUser = currentUser;
+      },
+      error: (err) => {
+        console.log('err', err);
+      },
+      complete: () => {},
+    });
   }
-
 }
