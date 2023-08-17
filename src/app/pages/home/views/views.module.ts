eeef3ff19d11c0e12a36c0b976d5d205353eb4be
Saied <elsaied.samaka1997@gmail.com>
2023-08-17 17:18:21 +0300

    breakpoint 15

--- src/app/pages/home/views/views.module.ts
+++ src/app/pages/home/views/views.module.ts
@@ -1,10 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { SharedModule } from 'src/app/shared/shared.module';
 import { CompanyCardComponent } from './company-card/company-card.component';
+import { CreatePostComponent } from './create-post/create-post.component';
 
 @NgModule({
-  imports: [CommonModule],
-  declarations: [CompanyCardComponent],
-  exports: [CompanyCardComponent],
+  imports: [CommonModule, SharedModule],
+  declarations: [CompanyCardComponent, CreatePostComponent],
+  exports: [CompanyCardComponent, CreatePostComponent],
 })
 export class ViewsModule {}
