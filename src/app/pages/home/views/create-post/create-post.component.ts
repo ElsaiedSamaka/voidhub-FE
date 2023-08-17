936948e566e39d2e0f7187e1e5a086125e67ea69
Saied <elsaied.samaka1997@gmail.com>
2023-08-17 21:29:34 +0300

    breakpoint 21

--- src/app/pages/home/views/create-post/create-post.component.ts
+++ src/app/pages/home/views/create-post/create-post.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-post',
   templateUrl: './create-post.component.html',
-  styleUrls: ['./create-post.component.css']
+  styleUrls: ['./create-post.component.css'],
 })
 export class CreatePostComponent implements OnInit {
+  showCreateNewPostModal: boolean = false;
+  validators = Validators;
+  isFormValid: boolean = false;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit() {}
+  openCreateNewPostModal() {
+    this.showCreateNewPostModal = true;
   }
-
+  closeCreateNewPostModal() {
+    this.showCreateNewPostModal = false;
+  }
 }
