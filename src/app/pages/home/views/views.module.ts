940495694be5c074b66f3f87099af9cdaf15447c
Saied <elsaied.samaka1997@gmail.com>
2023-08-17 17:19:46 +0300

    breakpoint 16

--- src/app/pages/home/views/views.module.ts
+++ src/app/pages/home/views/views.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CompanyCardComponent } from './company-card/company-card.component';
 import { CreatePostComponent } from './create-post/create-post.component';
+import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
-  declarations: [CompanyCardComponent, CreatePostComponent],
-  exports: [CompanyCardComponent, CreatePostComponent],
+  declarations: [CompanyCardComponent, CreatePostComponent, TimelineComponent],
+  exports: [CompanyCardComponent, CreatePostComponent, TimelineComponent],
 })
 export class ViewsModule {}
