879ae81f0607cd53d1e060a303190a891d98c13d
Saied <elsaied.samaka1997@gmail.com>
2023-08-19 14:14:48 +0300

    breakpoint

--- src/app/shared/article/article.component.ts
+++ src/app/shared/article/article.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-article',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./article.component.css'],
 })
 export class ArticleComponent implements OnInit {
+  @Input() article: any = {};
   showFollowingPopOver: boolean = false;
   showCommentInput: boolean = false;
   constructor() {}
