4841f511694f3a088b3e4dda1432e0418d4abfba
Saied <elsaied.samaka1997@gmail.com>
2023-08-19 13:30:17 +0300

    breakpoint

--- src/app/pages/home/views/timeline/timeline.component.ts
+++ src/app/pages/home/views/timeline/timeline.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
+import { PostsService } from 'src/core/services/posts.service';
 
 @Component({
   selector: 'app-timeline',
   templateUrl: './timeline.component.html',
-  styleUrls: ['./timeline.component.css']
+  styleUrls: ['./timeline.component.css'],
 })
 export class TimelineComponent implements OnInit {
-
-  constructor() { }
+  constructor(private postsService: PostsService) {}
 
   ngOnInit() {
+    this.getAll();
   }
-
+  getAll(): void {
+    this.postsService.getAll().subscribe({
+      next: (response) => {
+        console.log('timeline [getAll] response', response);
+      },
+      error: (err) => {},
+      complete: () => {},
+    });
+  }
 }
