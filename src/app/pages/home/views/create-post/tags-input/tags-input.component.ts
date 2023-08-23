15993c7830d5a3198e41e658373af90eee0fc39a
Saied <elsaied.samaka1997@gmail.com>
2023-08-23 22:05:01 +0300

    breakpoint 63

---
+++ src/app/pages/home/views/create-post/tags-input/tags-input.component.ts
@@ -0,0 +1,48 @@
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/shared/services/theme.service';
+
+@Component({
+  selector: 'app-tags-input',
+  templateUrl: './tags-input.component.html',
+  styleUrls: ['./tags-input.component.css'],
+})
+export class TagsInputComponent implements OnInit {
+  tag: string = '';
+  tagId: any;
+  showTagsDropDown: boolean = false;
+  theme: string = '';
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit() {
+    this.getCurrentTheme();
+  }
+  handleTagsChange(): void {
+    console.log('handleTagsChange');
+  }
+  handleUserSelect(value: any): void {
+    console.log('handleUserSelect', value);
+    //  const userId = event.target.value;
+    //  const isChecked = event.target.checked;
+    //  const user = this.users.find((u) => u.id == userId);
+    //  const selectedIndex = this.selectedUsers.findIndex((u) => u.id == userId);
+
+    //  if (isChecked) {
+    //    if (selectedIndex === -1) {
+    //      this.selectedUsers.push(user);
+    //    }
+    //    this.showDropdown = false;
+    //    this.email = '';
+    //  } else {
+    //    if (selectedIndex !== -1) {
+    //      this.selectedUsers.splice(selectedIndex, 1);
+    //    }
+    //    this.email = '';
+    //    this.showDropdown = false;
+    //  }
+  }
+  getCurrentTheme() {
+    this.themeService.theme$.subscribe((theme) => {
+      this.theme = theme;
+    });
+  }
+}
--- src/app/pages/home/views/views.module.ts
+++ src/app/pages/home/views/views.module.ts
@@ -1,13 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { CompanyCardComponent } from './company-card/company-card.component';
 import { CreatePostComponent } from './create-post/create-post.component';
+import { TagsInputComponent } from './create-post/tags-input/tags-input.component';
 import { TimelineComponent } from './timeline/timeline.component';
 
 @NgModule({
-  imports: [CommonModule, SharedModule],
-  declarations: [CompanyCardComponent, CreatePostComponent, TimelineComponent],
-  exports: [CompanyCardComponent, CreatePostComponent, TimelineComponent],
+  imports: [CommonModule, FormsModule, SharedModule],
+  declarations: [
+    CompanyCardComponent,
+    CreatePostComponent,
+    TimelineComponent,
+    TagsInputComponent,
+  ],
+  exports: [
+    CompanyCardComponent,
+    CreatePostComponent,
+    TimelineComponent,
+    TagsInputComponent,
+  ],
 })
 export class ViewsModule {}
