6062917fb54861365ddd5b87b1c7ba314f93aa87
Saied <elsaied.samaka1997@gmail.com>
2023-08-26 16:08:03 +0300

    breakpoint 16

--- src/app/shared/prose/prose.component.ts
+++ src/app/shared/prose/prose.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from '../services/theme.service';
 
 @Component({
   selector: 'app-prose',
   templateUrl: './prose.component.html',
-  styleUrls: ['./prose.component.css']
+  styleUrls: ['./prose.component.css'],
 })
 export class ProseComponent implements OnInit {
+  currentTheme: string = '';
 
-  constructor() { }
+  constructor(private themeService: ThemeService) {}
 
   ngOnInit() {
+    this.themeService.theme$.subscribe((theme) => {
+      this.currentTheme = theme;
+    });
   }
-
 }
