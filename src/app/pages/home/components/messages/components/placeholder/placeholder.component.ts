b7dd233ade34d288f5f2400b4e3e18bb6f1405b3
Saied <elsaied.samaka1997@gmail.com>
2023-09-01 19:35:37 +0300

    breakpoint 16

--- src/app/pages/home/components/messages/components/placeholder/placeholder.component.ts
+++ src/app/pages/home/components/messages/components/placeholder/placeholder.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/shared/services/theme.service';
+import { AuthService } from 'src/core/services/auth.service';
 
 @Component({
   selector: 'app-placeholder',
   templateUrl: './placeholder.component.html',
-  styleUrls: ['./placeholder.component.css']
+  styleUrls: ['./placeholder.component.css'],
 })
 export class PlaceholderComponent implements OnInit {
+  currentTheme: string = '';
+  currentUser: any;
 
-  constructor() { }
+  constructor(
+    private themeService: ThemeService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.themeService.theme$.subscribe((theme) => {
+      this.currentTheme = theme;
+    });
+    this.authService.currentUser$.subscribe({
+      next: (currentUser) => {
+        this.currentUser = currentUser;
+      },
+      error: (err) => {
+        console.log('err', err);
+      },
+      complete: () => {},
+    });
   }
-
 }
