0ce8e8895d828d30e6007485ecbcf635d15c992a
Saied <elsaied.samaka1997@gmail.com>
2023-09-01 11:47:48 +0300

    breakpoint 16

--- src/app/pages/home/components/messages/messages.module.ts
+++ src/app/pages/home/components/messages/messages.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { IndexComponent } from './components/index/index.component';
 import { MessagesRoutingModule } from './messages-routing.module';
 
 @NgModule({
   imports: [MessagesRoutingModule, CommonModule],
-  declarations: [],
+  declarations: [IndexComponent],
 })
 export class MessagesModule {}
