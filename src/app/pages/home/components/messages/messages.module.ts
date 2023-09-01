fc34cd05cde2f33ce3ddae02a47f9f2982bbe139
Saied <elsaied.samaka1997@gmail.com>
2023-09-01 19:50:40 +0300

    breakpoint 16

--- src/app/pages/home/components/messages/messages.module.ts
+++ src/app/pages/home/components/messages/messages.module.ts
@@ -1,10 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { IndexComponent } from './components/index/index.component';
+import { PlaceholderComponent } from './components/placeholder/placeholder.component';
 import { MessagesRoutingModule } from './messages-routing.module';
 
 @NgModule({
   imports: [MessagesRoutingModule, CommonModule],
-  declarations: [IndexComponent],
+  declarations: [IndexComponent, PlaceholderComponent],
 })
 export class MessagesModule {}
--- src/app/pages/home/components/messages/views/views.module.ts
+++ src/app/pages/home/components/messages/views/views.module.ts
@@ -4,5 +4,6 @@ import { NgModule } from '@angular/core';
 @NgModule({
   imports: [CommonModule],
   declarations: [],
+  providers: [],
 })
 export class ViewsModule {}
