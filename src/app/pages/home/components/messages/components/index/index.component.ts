e4a7994088a3f87d36ebf9b7a4fb3dea57f3ff7c
Saied <elsaied.samaka1997@gmail.com>
2023-09-01 19:24:52 +0300

    breakpoint 16

--- src/app/pages/home/components/messages/components/index/index.component.ts
+++ src/app/pages/home/components/messages/components/index/index.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/shared/services/theme.service';
+import { AuthService } from 'src/core/services/auth.service';
 
 @Component({
   selector: 'app-index',
   templateUrl: './index.component.html',
-  styleUrls: ['./index.component.css']
+  styleUrls: ['./index.component.css'],
 })
 export class IndexComponent implements OnInit {
+  currentTheme: string = '';
+  currentUser: any;
 
-  constructor() { }
+  constructor(
+    private themeService: ThemeService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.themeService.theme$.subscribe((theme) => {
+      this.currentTheme = theme;
+    });
+    this.authService.currentUser$.subscribe({
+      next: (currentUser) => {
+        this.currentUser = currentUser;
+      },
+      error: (err) => {
+        console.log('err', err);
+      },
+      complete: () => {},
+    });
   }
-
 }
