1c40503fd3803dce4958c9beffdf64998da96cc9
Saied <elsaied.samaka1997@gmail.com>
2023-09-08 10:19:37 +0300

    breakpoint 16

--- src/utils/directives/my-common.module.ts
+++ src/utils/directives/my-common.module.ts
@@ -1,10 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SortDirective } from './sort.directive';
+import { MyDirectiveDirective } from './my-directive.directive';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [SortDirective],
+  declarations: [	SortDirective,
+      MyDirectiveDirective
+   ],
   exports: [SortDirective],
 })
 export class MyCommonModule {}
