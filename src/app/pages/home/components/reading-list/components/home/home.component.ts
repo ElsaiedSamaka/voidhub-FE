556d95d0e9cc23823089c7b9b0d17dc30bd0ce8b
Saied <elsaied.samaka1997@gmail.com>
2023-09-09 20:01:27 +0300

    breakpoint 16

--- src/app/pages/home/components/reading-list/components/home/home.component.ts
+++ src/app/pages/home/components/reading-list/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemeService } from 'src/app/shared/services/theme.service';
 import { AuthService } from 'src/core/services/auth.service';
+import { PostsService } from 'src/core/services/posts.service';
 
 @Component({
   selector: 'app-home',
@@ -10,16 +11,35 @@ import { AuthService } from 'src/core/services/auth.service';
 export class HomeComponent implements OnInit {
   currentTheme: string = '';
   currentUser: any;
-  savedArticles: any[] = [{}, {}];
+  savedArticles: any[] = [];
   constructor(
     private themeService: ThemeService,
-    private authService: AuthService
+    private authService: AuthService,
+    private postsService: PostsService
   ) {}
 
   ngOnInit() {
+    this.getSavedPosts();
+    this.getCurrentTheme();
+    this.getCurrentUser();
+  }
+  getSavedPosts(): void {
+    this.postsService.getSaved().subscribe({
+      next: (savedArticles) => {
+        this.savedArticles = savedArticles;
+      },
+      error: (err) => {
+        console.log('error while retrieving saved articles', err);
+      },
+      complete: () => {},
+    });
+  }
+  getCurrentTheme(): void {
     this.themeService.theme$.subscribe((theme) => {
       this.currentTheme = theme;
     });
+  }
+  getCurrentUser(): void {
     this.authService.currentUser$.subscribe({
       next: (currentUser) => {
         this.currentUser = currentUser;
