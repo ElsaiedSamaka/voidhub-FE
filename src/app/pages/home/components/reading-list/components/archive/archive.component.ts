64598c20a41110951e195b15e67ae8538090458c
Saied <elsaied.samaka1997@gmail.com>
2023-09-09 22:23:43 +0300

    breakpoint 16

--- src/app/pages/home/components/reading-list/components/archive/archive.component.ts
+++ src/app/pages/home/components/reading-list/components/archive/archive.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/shared/services/theme.service';
 
 @Component({
   selector: 'app-archive',
   templateUrl: './archive.component.html',
-  styleUrls: ['./archive.component.css']
+  styleUrls: ['./archive.component.css'],
 })
 export class ArchiveComponent implements OnInit {
+  currentTheme: string = '';
 
-  constructor() { }
+  constructor(private themeService: ThemeService) {}
 
   ngOnInit() {
+    this.getCurrentTheme();
   }
-
+  getCurrentTheme(): void {
+    this.themeService.theme$.subscribe((theme) => {
+      this.currentTheme = theme;
+    });
+  }
 }
--- src/app/pages/home/components/reading-list/reading-list.module.ts
+++ src/app/pages/home/components/reading-list/reading-list.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import { ArchiveComponent } from './components/archive/archive.component';
 import { DetailedComponent } from './components/detailed/detailed.component';
 import { HomeComponent } from './components/home/home.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
@@ -14,6 +15,7 @@ import { ReadingListRoutingModule } from './reading-list-routing.module';
     PlaceholderComponent,
     NotFoundComponent,
     DetailedComponent,
+    ArchiveComponent,
   ],
 })
 export class ReadingListModule {}
