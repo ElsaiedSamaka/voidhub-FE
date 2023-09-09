78dbf1e32f36ca6ff38b96052cbd6fcf285d1bf8
Saied <elsaied.samaka1997@gmail.com>
2023-09-09 22:09:04 +0300

    breakpoint 16

--- src/app/pages/home/components/reading-list/components/detailed/detailed.component.ts
+++ src/app/pages/home/components/reading-list/components/detailed/detailed.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ThemeService } from 'src/app/shared/services/theme.service';
 
 @Component({
   selector: 'app-detailed',
   templateUrl: './detailed.component.html',
-  styleUrls: ['./detailed.component.css']
+  styleUrls: ['./detailed.component.css'],
 })
 export class DetailedComponent implements OnInit {
+  data: any = {};
+  currentTheme: string = '';
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private themeService: ThemeService
+  ) {}
 
   ngOnInit() {
+    this.getBlog();
+    this.themeService.theme$.subscribe((theme) => {
+      this.currentTheme = theme;
+    });
   }
-
+  getBlog() {
+    this.route.data.subscribe((data) => {
+      this.data = data;
+    });
+  }
 }
