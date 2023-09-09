d975352de6cbdbc896bb59c7db5e63e9bd9c0d78
Saied <elsaied.samaka1997@gmail.com>
2023-09-09 15:03:06 +0300

    breakpoint 16

--- src/app/pages/home/components/reading-list/reading-list-routing.module.ts
+++ src/app/pages/home/components/reading-list/reading-list-routing.module.ts
@@ -1,22 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DetailedComponent } from './components/detailed/detailed.component';
 import { HomeComponent } from './components/home/home.component';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { PlaceholderComponent } from './components/placeholder/placeholder.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    children: [
-      { path: 'not-found', component: NotFoundComponent },
-      {
-        path: ':id',
-        component: DetailedComponent,
-      },
-      { path: '', component: PlaceholderComponent },
-    ],
+    // children: [
+    //   { path: '', component: PlaceholderComponent },
+    //   { path: 'not-found', component: NotFoundComponent },
+    //   {
+    //     path: ':id',
+    //     component: DetailedComponent,
+    //   },
+    // ],
   },
 ];
 
--- src/app/pages/home/components/reading-list/reading-list.module.ts
+++ src/app/pages/home/components/reading-list/reading-list.module.ts
@@ -1,9 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { DetailedComponent } from './components/detailed/detailed.component';
+import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PlaceholderComponent } from './components/placeholder/placeholder.component';
 import { ReadingListRoutingModule } from './reading-list-routing.module';
 
 @NgModule({
   imports: [CommonModule, ReadingListRoutingModule],
-  declarations: [],
+  declarations: [
+    HomeComponent,
+    PlaceholderComponent,
+    NotFoundComponent,
+    DetailedComponent,
+  ],
 })
 export class ReadingListModule {}
