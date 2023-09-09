f66636313e1370214d9c474f4e7bb3906f58f84b
Saied <elsaied.samaka1997@gmail.com>
2023-09-09 15:04:41 +0300

    breakpoint 16

--- src/app/pages/home/components/reading-list/reading-list-routing.module.ts
+++ src/app/pages/home/components/reading-list/reading-list-routing.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DetailedComponent } from './components/detailed/detailed.component';
 import { HomeComponent } from './components/home/home.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { PlaceholderComponent } from './components/placeholder/placeholder.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
-    // children: [
-    //   { path: '', component: PlaceholderComponent },
-    //   { path: 'not-found', component: NotFoundComponent },
-    //   {
-    //     path: ':id',
-    //     component: DetailedComponent,
-    //   },
-    // ],
+    children: [
+      { path: '', component: PlaceholderComponent },
+      { path: 'not-found', component: NotFoundComponent },
+      {
+        path: ':id',
+        component: DetailedComponent,
+      },
+    ],
   },
 ];
 
