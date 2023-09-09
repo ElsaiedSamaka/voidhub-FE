d9a8695faef9ddc0f4182234135aac7bbfbb53fc
Saied <elsaied.samaka1997@gmail.com>
2023-09-09 22:15:30 +0300

    breakpoint 16

--- src/app/pages/home/components/reading-list/components/placeholder/placeholder.component.ts
+++ src/app/pages/home/components/reading-list/components/placeholder/placeholder.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/shared/services/theme.service';
 
 @Component({
   selector: 'app-placeholder',
   templateUrl: './placeholder.component.html',
-  styleUrls: ['./placeholder.component.css']
+  styleUrls: ['./placeholder.component.css'],
 })
 export class PlaceholderComponent implements OnInit {
+  currentTheme: string = '';
 
-  constructor() { }
+  constructor(private themeService: ThemeService) {}
 
   ngOnInit() {
+    this.getCurrentTheme();
   }
-
+  getCurrentTheme(): void {
+    this.themeService.theme$.subscribe((theme) => {
+      this.currentTheme = theme;
+    });
+  }
 }
