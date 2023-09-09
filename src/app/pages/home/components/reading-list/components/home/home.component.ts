8ee74cc9ca0464fdcf0009f434f88e7744cd4607
Saied <elsaied.samaka1997@gmail.com>
2023-09-09 15:18:30 +0300

    breakpoint 16

--- src/app/pages/home/components/reading-list/components/home/home.component.ts
+++ src/app/pages/home/components/reading-list/components/home/home.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/shared/services/theme.service';
+import { AuthService } from 'src/core/services/auth.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
-
-  constructor() { }
+  currentTheme: string = '';
+  currentUser: any;
+  constructor(
+    private themeService: ThemeService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.themeService.theme$.subscribe((theme) => {
+      this.currentTheme = theme;
+    });
+    this.authService.currentUser$.subscribe({
+      next: (currentUser) => {
+        this.currentUser = currentUser;
+      },
+      error: (err) => {
+        console.log('err', err);
+      },
+      complete: () => {},
+    });
   }
-
 }
