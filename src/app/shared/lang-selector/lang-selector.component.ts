dd8030c84caadee00d0451702790a696a4b140c3
Saied <elsaied.samaka1997@gmail.com>
2023-09-10 22:37:58 +0300

    breakpoint 17

--- src/app/shared/lang-selector/lang-selector.component.ts
+++ src/app/shared/lang-selector/lang-selector.component.ts
@@ -8,21 +8,21 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class LangSelectorComponent implements OnInit {
   showLangDDL: boolean = false;
-  selectedLang: string;
+  DEFAULT_LANGUAGE: string;
 
   constructor(private translate: TranslateService) {
     // Get the language from local storage or set a default value
-    this.selectedLang = localStorage.getItem('selectedLang') || 'en';
+    this.DEFAULT_LANGUAGE = localStorage.getItem('DEFAULT_LANGUAGE') || 'en';
   }
 
   ngOnInit() {
-    this.translate.use(this.selectedLang);
+    this.translate.use(this.DEFAULT_LANGUAGE);
   }
 
   changeLanguage(language: string) {
     this.translate.use(language);
-    this.selectedLang = language;
-    localStorage.setItem('selectedLang', language);
+    this.DEFAULT_LANGUAGE = language;
+    localStorage.setItem('DEFAULT_LANGUAGE', language);
     this.toggleLangDDL();
   }
 
