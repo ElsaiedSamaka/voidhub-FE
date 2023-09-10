00071943bf3fa353bd43ae7f678a90177e6724ce
Saied <elsaied.samaka1997@gmail.com>
2023-09-10 22:28:36 +0300

    breakpoint 17

--- src/app/shared/lang-selector/lang-selector.component.ts
+++ src/app/shared/lang-selector/lang-selector.component.ts
@@ -24,6 +24,7 @@ export class LangSelectorComponent implements OnInit {
       default:
         break;
     }
+    this.toggleLangDDL();
   }
   toggleLangDDL(): void {
     this.showLangDDL = !this.showLangDDL;
