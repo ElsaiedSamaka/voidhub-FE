dfe9b2889c83d0204fc78898f4cf716bfe72d589
Saied <elsaied.samaka1997@gmail.com>
2023-09-12 07:57:17 +0300

    breakpoint 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
+import { LanguageService } from 'src/core/services/language.service';
 import { SocketService } from 'src/core/services/socket.service';
 import { LoadingService } from './shared/services/loading.service';
 import { ThemeService } from './shared/services/theme.service';
@@ -11,22 +12,35 @@ import { ThemeService } from './shared/services/theme.service';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
-  title = 'VoidHub';
   loading$: Observable<boolean>;
 
   constructor(
     private loadingService: LoadingService,
     private themeService: ThemeService,
+    private languageService: LanguageService,
     private socketService: SocketService,
-    private translate: TranslateService
+    private translate: TranslateService,
+    private renderer: Renderer2
   ) {
     this.loading$ = this.loadingService.loading$;
   }
   ngOnInit(): void {
-    // this.dataService.subscribeToTheme();
     this.themeService.getCurrentTheme();
+    this.getCurrentLanguage();
   }
-  // changeLanguage(language: string) {
-  //   this.translate.use(language);
-  // }
+  getCurrentLanguage(): void {
+    this.languageService.currentLanguage$.subscribe((language) => {
+      const isRTL = language === 'ar';
+
+      // Set lang attribute
+      this.renderer.setAttribute(document.documentElement, 'lang', language);
+
+      // Set dir attribute
+      this.renderer.setAttribute(
+        document.documentElement,
+        'dir',
+        isRTL ? 'rtl' : 'ltr'
+      );
+    });
+  }
 }
