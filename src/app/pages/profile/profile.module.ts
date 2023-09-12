d262ea717b33404d91a24b5b8a604691dd9a3ae1
Saied <elsaied.samaka1997@gmail.com>
2023-09-12 21:24:06 +0300

    breakpoint 17

--- src/app/pages/profile/profile.module.ts
+++ src/app/pages/profile/profile.module.ts
@@ -1,11 +1,25 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpLoaderFactory } from 'src/app/app.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeComponent } from './components/home/home.component';
 import { ProfileRoutingModule } from './profile-routing.module';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, ProfileRoutingModule],
+  imports: [
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    CommonModule,
+    SharedModule,
+    ProfileRoutingModule,
+  ],
   declarations: [HomeComponent],
 })
 export class ProfileModule {}
