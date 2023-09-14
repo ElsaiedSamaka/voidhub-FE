11fe99ed84676aa6cc4ef4ef7cec4a6de185fc01
Saied <elsaied.samaka1997@gmail.com>
2023-09-14 17:20:57 +0300

    breakpoint 17

--- src/app/pages/home/views/comment-section/reply/reply.component.ts
+++ src/app/pages/home/views/comment-section/reply/reply.component.ts
@@ -1,14 +1,61 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
+import { RepliesService } from 'src/core/services/replies.service';
 
 @Component({
   selector: 'app-reply',
   templateUrl: './reply.component.html',
   styleUrls: ['./reply.component.css'],
 })
-export class ReplyComponent implements OnInit {
+export class ReplyComponent implements OnInit, OnChanges {
   @Input() currentTheme: string = '';
   @Input() reply: any;
-  constructor() {}
+  @Output() emitRemovedReply: EventEmitter<any> = new EventEmitter<any>();
+  @Output() emitEditReply: EventEmitter<any> = new EventEmitter<any>();
+  showActionsDDL: boolean = false;
+
+  constructor(private repliesService: RepliesService) {}
 
   ngOnInit() {}
+  ngOnChanges(changes: SimpleChanges): void {
+    if (changes.reply && changes.reply.currentValue) {
+      // Update the reply value
+      this.reply = changes.reply.currentValue;
+    }
+  }
+  handleActions(event: any): void {
+    event.stopPropagation();
+    this.showActionsDDL = !this.showActionsDDL;
+  }
+  handleReplyRemove(event: any): void {
+    event.stopPropagation();
+    this.showActionsDDL = false;
+    this.repliesService.deleteById(this.reply.id).subscribe({
+      next: (removedReply) => {
+        this.emitRemovedReply.emit(removedReply);
+      },
+      error: (err) => {
+        console.log(
+          'error while removeing comment [actions-icon component]',
+          err
+        );
+      },
+      complete: () => {},
+    });
+  }
+  // handleCommentReport(event: any): void {
+  //   event.stopPropagation();
+  //   this.showActionsDDL = false;
+  //   console.log('handleCommentReport');
+  // }
+  // handleCommentEdit(event: any): void {
+  //   this.emitEditComment.emit(this.comment);
+  // }
 }
