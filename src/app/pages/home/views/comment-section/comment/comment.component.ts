35c74b38dfa2fd4ee3d78f49e2297578024487b4
Saied <elsaied.samaka1997@gmail.com>
2023-09-14 07:02:58 +0300

    breakpoint 17

--- src/app/pages/home/views/comment-section/comment/comment.component.ts
+++ src/app/pages/home/views/comment-section/comment/comment.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { CommentsService } from 'src/core/services/comments.service';
 
 @Component({
   selector: 'app-comment',
@@ -9,11 +10,35 @@ export class CommentComponent implements OnInit {
   @Input() currentTheme: string = '';
   @Input() comment: any;
   showFollowingPopOver: boolean = false;
+  showActionsDDL: boolean = false;
 
-  constructor() {}
+  constructor(private commentsService: CommentsService) {}
 
   ngOnInit() {}
   toggleFollowingPopOver() {
     this.showFollowingPopOver = !this.showFollowingPopOver;
+  }
+  handleActions(event: any): void {
+    event.stopPropagation();
+    this.showActionsDDL = !this.showActionsDDL;
+  }
+  handlePostRemove(event: any): void {
+    event.stopPropagation();
+    this.showActionsDDL = false;
+    this.commentsService.deleteById(this.comment.id).subscribe({
+      next: (removedComment) => {},
+      error: (err) => {
+        console.log(
+          'error while removeing comment [actions-icon component]',
+          err
+        );
+      },
+      complete: () => {},
+    });
+  }
+  handlePostReport(event: any): void {
+    event.stopPropagation();
+    this.showActionsDDL = false;
+    console.log('handlePostReport');
   }
 }
