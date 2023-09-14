9e573d69981ca84beafb7a456937068b4def626f
Saied <elsaied.samaka1997@gmail.com>
2023-09-14 20:21:43 +0300

    breakpoint 17

--- src/app/pages/home/components/companies/companies.component.ts
+++ src/app/pages/home/components/companies/companies.component.ts
@@ -1,15 +1,32 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/shared/services/theme.service';
+import { AuthService } from 'src/core/services/auth.service';
 
 @Component({
   selector: 'app-companies',
   templateUrl: './companies.component.html',
-  styleUrls: ['./companies.component.css']
+  styleUrls: ['./companies.component.css'],
 })
 export class CompaniesComponent implements OnInit {
-
-  constructor() { }
+  currentTheme: string = '';
+  currentUser: any;
+  constructor(
+    private themeService: ThemeService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
+    this.themeService.theme$.subscribe((theme) => {
+      this.currentTheme = theme;
+    });
+    this.authService.currentUser$.subscribe({
+      next: (currentUser) => {
+        this.currentUser = currentUser;
+      },
+      error: (err) => {
+        console.log('err', err);
+      },
+      complete: () => {},
+    });
   }
-
 }
