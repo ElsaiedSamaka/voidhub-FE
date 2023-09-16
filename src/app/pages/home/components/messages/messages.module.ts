220aa9dd677836e15082d544cb162cf2248a5558
Saied <elsaied.samaka1997@gmail.com>
2023-09-16 14:13:26 +0300

    breakpoint 17

--- src/app/pages/home/components/messages/messages.module.ts
+++ src/app/pages/home/components/messages/messages.module.ts
@@ -1,6 +1,9 @@
 import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { HttpLoaderFactory } from 'src/app/app.module';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { DetailedComponent } from './components/detailed/detailed.component';
 import { IndexComponent } from './components/index/index.component';
@@ -10,7 +13,19 @@ import { SettingsComponent } from './components/settings/settings.component';
 import { MessagesRoutingModule } from './messages-routing.module';
 
 @NgModule({
-  imports: [MessagesRoutingModule, SharedModule, CommonModule, FormsModule],
+  imports: [
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+    MessagesRoutingModule,
+    SharedModule,
+    CommonModule,
+    FormsModule,
+  ],
   declarations: [
     IndexComponent,
     PlaceholderComponent,
