dc14bb6a14559e776654f143a4698c4a3e415483
Saied <elsaied.samaka1997@gmail.com>
2023-09-16 23:44:13 +0300

    breakpoint 17

--- src/app/pages/profile/components/home/home.component.ts
+++ src/app/pages/profile/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThemeService } from 'src/app/shared/services/theme.service';
 import { AuthService } from 'src/core/services/auth.service';
 import { PostsService } from 'src/core/services/posts.service';
 
@@ -8,15 +9,18 @@ import { PostsService } from 'src/core/services/posts.service';
   styleUrls: ['./home.component.css'],
 })
 export class HomeComponent implements OnInit {
+  currentTheme: string = '';
   currentUser: any;
   posts: any[] = [];
   constructor(
     private authService: AuthService,
-    private postsService: PostsService
+    private postsService: PostsService,
+    private themeService: ThemeService
   ) {}
 
   ngOnInit() {
     this.getCurrentUser();
+    this.getCurrentTheme();
     this.getPostsByUseId();
   }
   getCurrentUser(): void {
@@ -28,6 +32,11 @@ export class HomeComponent implements OnInit {
         console.log('err', err);
       },
       complete: () => {},
+    });
+  }
+  getCurrentTheme(): void {
+    this.themeService.theme$.subscribe((theme) => {
+      this.currentTheme = theme;
     });
   }
   getPostsByUseId(): void {
