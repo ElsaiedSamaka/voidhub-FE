638742f11126da8835ee2a2b38a000c0df85d739
Saied <elsaied.samaka1997@gmail.com>
2023-09-17 02:06:46 +0300

    breakpoint 17

--- src/app/pages/settings/components/home/home.component.ts
+++ src/app/pages/settings/components/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators } from '@angular/forms';
 import { ThemeService } from 'src/app/shared/services/theme.service';
 import { AuthService } from 'src/core/services/auth.service';
+import { UsersService } from 'src/core/services/users.service';
 
 @Component({
   selector: 'app-home',
@@ -10,9 +12,12 @@ import { AuthService } from 'src/core/services/auth.service';
 export class HomeComponent implements OnInit {
   currentTheme: string = '';
   currentUser: any;
+  validators = Validators;
+  isFormValid: boolean = false;
   constructor(
     private themeService: ThemeService,
-    private authService: AuthService
+    private authService: AuthService,
+    private usersService: UsersService
   ) {}
 
   ngOnInit() {
@@ -28,5 +33,54 @@ export class HomeComponent implements OnInit {
       },
       complete: () => {},
     });
+  }
+  updateProfileImg(): void {
+    const currentUserID = this.currentUser.id;
+    this.usersService.put(currentUserID, {}).subscribe({
+      next: (response) => {
+        console.log('response', response);
+      },
+      error: (err) => {
+        console.log('err', err);
+      },
+      complete: () => {},
+    });
+  }
+  updateInfo(): void {
+    const currentUserID = this.currentUser.id;
+    this.usersService.put(currentUserID, {}).subscribe({
+      next: (response) => {
+        console.log('response', response);
+      },
+      error: (err) => {
+        console.log('err', err);
+      },
+      complete: () => {},
+    });
+  }
+  checkFormStatus(value: any) {
+    switch (value) {
+      case 'INVALID':
+        this.isFormValid = false;
+        break;
+      case 'VALID':
+        this.isFormValid = true;
+        break;
+
+      default:
+        break;
+    }
+  }
+  onFormSubmitted(article: any) {
+    if (!this.isFormValid) return;
+    // let tagsIds = this.receivedTags.map((tag) => tag.id);
+    // let content = this.receivedHTMLContent;
+    // let userId = this.currentUser.id;
+    // let isAnonymous = this.dataService.isAnonymous$.value;
+    // article.append('tagsIds', tagsIds);
+    // article.append('content', content);
+    // article.append('userId', userId);
+    // article.append('isAnonymous', isAnonymous);
+    // this.postArticle(article);
   }
 }
--- src/app/pages/settings/settings.module.ts
+++ src/app/pages/settings/settings.module.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { HomeComponent } from './components/home/home.component';
 import { SettingsRoutingModule } from './settings-routing.module';
 
 @NgModule({
-  imports: [CommonModule, SharedModule, SettingsRoutingModule],
+  imports: [
+    TranslateModule, // Import TranslateModule here
+    CommonModule,
+    SharedModule,
+    SettingsRoutingModule,
+  ],
   declarations: [HomeComponent],
 })
 export class SettingsModule {}
