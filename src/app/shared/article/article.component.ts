53974419c27f3b4d62d796b9383f894fa2f284f1
Saied <elsaied.samaka1997@gmail.com>
2023-09-16 22:40:32 +0300

    breakpoint 17

--- src/app/shared/article/article.component.ts
+++ src/app/shared/article/article.component.ts
@@ -27,6 +27,8 @@ export class ArticleComponent implements OnInit, OnChanges {
   saveCount: number = 0;
   loveCount: number = 0;
   noOfComments: number = 0;
+  followingCount: number = 0;
+  followersCount: number = 0;
   constructor(
     private commentsService: CommentsService,
     private postsService: PostsService,
@@ -41,10 +43,12 @@ export class ArticleComponent implements OnInit, OnChanges {
     this.noOfComments = comments.length;
     // check if current already follow the creator of the article
     this.isFollowing();
+    this.followingCount = this.article.user.following.length;
+    this.followersCount = this.article.user.follower.length;
   }
   ngOnChanges(changes: SimpleChanges): void {
     if (changes.article && changes.article.currentValue) {
-      // Update the reply value
+      // Update the article value
       this.article = changes.article.currentValue;
     }
   }
@@ -161,12 +165,14 @@ export class ArticleComponent implements OnInit, OnChanges {
       followerId: followerId,
       followingId: followingId,
     };
-    this.usersService
-      .follow(userToFollow)
-      .subscribe({
-        next: () => {
+    this.usersService.follow(userToFollow).subscribe({
+      next: () => {
         this.isFollowing = () => true;
-      }, error: () => {}, complete: () => {} });
+        this.followersCount = this.followersCount + 1;
+      },
+      error: () => {},
+      complete: () => {},
+    });
   }
   unfollow(event: any, followingId: any): void {
     event.stopPropagation();
@@ -178,6 +184,7 @@ export class ArticleComponent implements OnInit, OnChanges {
     this.usersService.unfollow(userToUnFollow).subscribe({
       next: () => {
         this.isFollowing = () => false;
+        this.followersCount = this.followersCount - 1;
       },
       error: () => {},
       complete: () => {},
