c1a5775fb0832c64781c625adb2d4fd370932448
Saied <elsaied.samaka1997@gmail.com>
2023-09-30 18:19:45 +0300

    breakpoint 18

--- src/app/pages/home/components/index/index.component.ts
+++ src/app/pages/home/components/index/index.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { debounceTime } from 'rxjs';
 import { ThemeService } from 'src/app/shared/services/theme.service';
 import { AuthService } from 'src/core/services/auth.service';
+import { ChatService } from 'src/core/services/chat.service';
 import { SocketService } from 'src/core/services/socket.service';
 
 @Component({
@@ -11,11 +13,13 @@ import { SocketService } from 'src/core/services/socket.service';
 export class IndexComponent implements OnInit {
   currentUser: any;
   currentTheme: string = '';
+  conversations: any[] = [];
 
   constructor(
     private themeService: ThemeService,
     private authService: AuthService,
-    private socketService: SocketService
+    private socketService: SocketService,
+    private chatService: ChatService
   ) {}
 
   ngOnInit() {
@@ -32,11 +36,34 @@ export class IndexComponent implements OnInit {
       complete: () => {},
     });
     this.emmitCurrentUser();
+    this.getConversations();
   }
   // emmit current User
   emmitCurrentUser() {
     this.socketService.socket.emit('currentUser', {
       userId: this.currentUser.id,
+    });
+  }
+  getConversations() {
+    this.chatService.getConversations(this.currentUser.id);
+    this.chatService.conversations$.pipe(debounceTime(100)).subscribe(() => {
+      this.conversations = this.chatService.conversations$.value;
+      this.joinRooms();
+      this.leaveRooms();
+    });
+  }
+  joinRooms(): void {
+    console.log('joinRooms called');
+    const userId = this.currentUser.id;
+    this.conversations.forEach((conversation) => {
+      this.chatService.joinRoom(conversation.id, userId);
+    });
+  }
+  leaveRooms() {
+    console.log('leaveRooms called');
+    const userId = this.currentUser.id;
+    this.conversations.forEach((conversation) => {
+      this.chatService.leaveRoom(conversation.id, userId);
     });
   }
 }
